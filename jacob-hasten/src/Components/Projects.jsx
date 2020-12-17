170c6b1b7d133705df4502a05b0d5af86360c042
Izzy Hasten <izzyhasten@gmail.com>
2020-12-17 14:31:39 -0500

    projects modal mobile

--- jacob-hasten/src/Components/Projects.jsx
+++ jacob-hasten/src/Components/Projects.jsx
@@ -1,29 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import landingPageCard from "../imgs/landing-page-card.png";
-import RejoycePageCard from "../imgs/rejoyce-img.jpg"
+import RejoycePageCard from "../imgs/rejoyce-img.jpg";
+import todoAppCard from "../imgs/todoapp.png";
+import Modal from "react-modal";
+import MediaQuery from 'react-responsive';
+import RejoyceModal from './RejoyceModal';
 
 function Projects() {
+
+    const [modalRejoyceIsOpen, setModalRejoyceIsOpen] = useState(false)
+    const [modalLandingPageIsOpen, setModalLandingPageIsOpen] = useState(false)
+    const [modalTodoAppIsOpen, setModalTodoAppIsOpen] = useState(false)
+
+    const mobileProjectsModalStyle =
+    {
+        overlay: {
+            position: null,
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: null,
+        },
+        content: {
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 200,
+            border: null,
+            background: '#021642',
+            overflow: 'auto',
+            WebkitOverflowScrolling: 'touch',
+            borderRadius: '4px',
+            outline: 'none',
+            padding: null,
+        }
+    }
+
+
     return (
         <>
-        <div className="title-container">
-        <h1>Projects</h1>
-        </div>
-        <div className="projects-container">
-            <div className="projects-container-all-cards">
-                <div className="projects-container-card">
-                    <button className="projects-card-title">Rejoyce</button>
-                    <img className="projects-card-img" src={RejoycePageCard} alt="landing-page-img" />
-                </div>
-                <div className="projects-container-card">
-                    <button className="projects-card-title">Landing Page</button>
-                    <img className="projects-card-img" src={landingPageCard} alt="landing-page-img" />
-                </div>
-                <div className="projects-container-card">
-                    <button className="projects-card-title">Landing Page</button>
-                    <img className="projects-card-img" src={landingPageCard} alt="landing-page-img" />
+            <div className="page-title-container">
+                <h1 className="page-title">Projects</h1>
+            </div>
+            <div className="projects-container">
+                <div className="projects-container-all-cards">
+                    <button onClick={() => setModalRejoyceIsOpen(true)} className="projects-container-card">
+                        <button className="projects-card-title">Rejoyce</button>
+                        <img className="projects-card-img" src={RejoycePageCard} alt="landing-page-img" />
+                    </button>
+                    <button onClick={() => setModalLandingPageIsOpen(true)} className="projects-container-card">
+                        <button className="projects-card-title">Landing Page</button>
+                        <img className="projects-card-img" src={landingPageCard} alt="landing-page-img" />
+                    </button>
+                    <button onClick={() => setModalTodoAppIsOpen(true)} className="projects-container-card">
+                        <button className="projects-card-title">Todo App</button>
+                        <img className="projects-card-img" src={todoAppCard} alt="todo-app-img" />
+                    </button>
+
                 </div>
             </div>
-        </div>
+            <MediaQuery maxDeviceWidth={900}>
+                <Modal style={mobileProjectsModalStyle} isOpen={modalRejoyceIsOpen}>
+                    <RejoyceModal updateModal={setModalRejoyceIsOpen}/>
+                </Modal>
+                <Modal isOpen={modalLandingPageIsOpen}>
+                <h1>Hi</h1>
+                </Modal>
+                <Modal isOpen={modalTodoAppIsOpen}>
+                <h1>Hi</h1>
+                </Modal>
+            </MediaQuery>
+            <MediaQuery minDeviceWidth={901}>
+            <Modal isOpen={modalRejoyceIsOpen}>
+                    <h1>Big Boy</h1>
+                </Modal>
+
+            </MediaQuery>
         </>
     )
 }
---
+++ jacob-hasten/src/Components/RejoyceModal.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import close from "../imgs/close_24px_outlined.svg";
+
+function RejoyceModal(props) {
+    return (
+        <>
+            < div className="home-container-hamburger">
+                <img onClick={() => props.updateModal(false)} className="nav-close-tab" src={close} alt="close-tab" />
+            </div>
+            <div className="projects-modal-container">
+                <h3>Rejoyce was a four-week collaboration project with the goal of designing and developing a minimum viable product addressing a specific problem:</h3>
+                <br />
+                <h4>"In recent events, parents are finding it hard to come up with ideas for keeping their children distracted at home during the day  while trying to work. "</h4>
+                <br />
+                <h3>
+
+                    Check out a more comprehensive case study <a className="description-link" href="https://beckneruie.wordpress.com/2020/10/22/rejoyce-app-project/" target="_blank" rel="noreferrer">here.</a>
+                    <br />
+                </h3>
+                <br />
+                <h4>My specific roles in this project were the following:</h4>
+                <ul className="projects-modal-list">
+                    <li> During the discovery process, I wrote out interview questions for potential 
+                        users that were non-leading in order to discover common problems and behavior 
+                        patterns among parents being stuck at home with their children.</li>
+                    <li> Once we began designing the application, I took a heavy role in the brainstorm
+                         sessions, designing low-fidelity wireframes, and creating the architecture of the 
+                         application. </li>
+                    <li> During the development process I was the product owner. 
+                        We used ReactJS to create the minimum viable product. I hosted the project on Github, 
+                        which gave me the responsibility to resolve merge conflicts, divide up the coding tasks 
+                        among the team, and to constantly communicate and update the team members on new pull requests.
+                    I built the state in the application, created the routing, wrote most of the Javascript aside 
+                    from the timer component, and deployed the website via netlify.</li>
+                    <li> When the project was due for presentation, I led it.</li>
+
+                </ul>
+        <div className="projects-modal-github-button-container">
+                <button> <a href="https://github.com/Jacobhasten/kenzie-collab-team3" target="_blank" rel="noreferrer" className="projects-modal-github-button">Github Repo</a></button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default RejoyceModal;
